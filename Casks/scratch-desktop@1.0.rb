e142ced9c3bbbbbec96b284b94372cfe3e90e867
GitLab CI <ci@whalesync.com>
2026-06-19 04:58:04 +0000

    Scratch Desktop v1.0.39

--- Casks/scratch-desktop@1.0.rb
+++ Casks/scratch-desktop@1.0.rb
@@ -1,6 +1,6 @@
 cask "scratch-desktop@1.0" do
   version "1.0.39"
-  sha256 "74670deda419c9219fda11a9b7273a6945592665d5aec3cd886e24ba9a82fd1f"
+  sha256 "e63debacaf953636b91ada7c95e5e79617ba3f4c579a1cfbbc92ec73b2bbd7a8"
 
   url "https://github.com/whalesync/scratch-desktop/releases/download/v1.0.39/Scratch-1.0.39-arm64.zip"
   name "Scratch Desktop"
