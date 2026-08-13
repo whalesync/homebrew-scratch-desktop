8b459e56df48d0576494720fa4a31a151f8ed5bc
GitLab CI <ci@whalesync.com>
2026-08-13 16:56:32 +0000

    Scratch Desktop v1.0.103

--- Casks/scratch-desktop.rb
+++ Casks/scratch-desktop.rb
@@ -1,8 +1,8 @@
 cask "scratch-desktop" do
-  version "1.0.101"
-  sha256 "9112b3857919cc50aea73e59f2eddb1397df4d95c5d89086aa308983a4a59821"
+  version "1.0.103"
+  sha256 "4d1781ea7809d1d1534f58d1f5539484fae9959fc194dab6f50fa85acdd1128e"
 
-  url "https://github.com/whalesync/scratch-desktop/releases/download/v1.0.101/Scratch-1.0.101-arm64.zip"
+  url "https://github.com/whalesync/scratch-desktop/releases/download/v1.0.103/Scratch-1.0.103-arm64.zip"
   name "Scratch Desktop"
   desc "Scratch content management desktop app"
   homepage "https://github.com/whalesync/scratch-desktop"
--- Casks/scratch-desktop@1.0.103.rb
+++ Casks/scratch-desktop@1.0.103.rb
@@ -1,4 +1,4 @@
-cask "scratch-desktop@1" do
+cask "scratch-desktop@1.0.103" do
   version "1.0.103"
   sha256 "4d1781ea7809d1d1534f58d1f5539484fae9959fc194dab6f50fa85acdd1128e"
 
